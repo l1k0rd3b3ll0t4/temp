9a715f6d4df2874f59cc1c754a66dcd333a5741d
root <root@localhost.localdomain>
2017-12-13 09:54:16 +0100

    commit2

---
+++ Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Desarrollar una aplicacion con las siguientes funcionalidades
+ * 1. Al al pulsar el boton Establecer nombre d efichero y mediante un JOptionPane la aplicacion solicita un fichero 
+que usara la aplicacion
+    2. Una etiqueta debe mostrar el nombre del fichero de texto, introducido en el punto 1. De igual manera, otra etiqueta debe dar informacion
+del dato
+    3. Uso de un slider  para gestionar un numero entero ente 0 y 10000.
+    El numero debe ser una etiqueta cerca del slider resaltada 
+    10 Radio Buttons para elegir la cantidad de numeros aleatorios por linea. Solo uno de ellos debe de estar activado.
+    Solo uno de ellos pulsado
+    4. En la parte superior del frame debe apacerer tu nombre completo detro de una etiqueta Añadir el rotulo añadir fichero (boton) 
+Como respuesta del evento onClick  debe generar un fichero de texto que contendrá tantas lineas que indique la etiqueta del apartado tres.
+Cada linea debe contener un numero ded orden seguiro del caracter : y una secuencia elegible de 1 a 10 numeros aleatorioas entre 1 y 48
+ */
+package recuperacion_interfaces;
+
+/**
+ *
+ * @author root
+ */
+public class Principal {
+    
+}
